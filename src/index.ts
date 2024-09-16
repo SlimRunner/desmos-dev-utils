a2fbc7162885393ffd8cb4051a5d832bd78d3cff
SlimRunner <dv.polygon@gmail.com>
2024-09-16 13:27:15 -0700

    Add a title changer function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Empty Userscript");
+import { calculator } from "Calc";
+
+class Desv {
+  changeTitle(title: string) {
+    calculator._calc.globalHotkeys.mygraphsController.graphsController.currentGraph.setProperty(
+      "title",
+      title
+    );
+  }
+}
+
+declare global {
+  interface Window {
+    desv: Desv
+  }
+}
+
+window.desv = new Desv();
+
